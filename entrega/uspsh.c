983acea1f929bae789ecb2009d1f462b12cc06a6
Matheus Silver <matheus.feitosa16@outlook.com>
2025-02-24 20:30:18 -0300

    update whoami

--- entrega/uspsh.c
+++ entrega/uspsh.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <pwd.h>
 
 #define MACHINE_NAME_MAX 16
 #define PATH_NAME_MAX 255
@@ -14,17 +15,16 @@ void get_hostname(char *hostname, int size) {
     gethostname(hostname, size);
 }
 
-void print_shell_prompt(const char *hostname, const char *currentDirectory) {
-    printf("[%s:%s]$ ", hostname, currentDirectory);
+char* get_curdir() {
+    return getenv("PWD");
 }
 
-void get_current_directory(char *currentDirectory, int size) {
-    getcwd(currentDirectory, size);
+void print_shell_prompt(const char *hostname) {
+    printf("[%s:%s]$ ", hostname, get_curdir());
 }
 
-void change_directory(const char *new_directory, char *currentDirectory, int size){
-    chdir(new_directory);    
-    get_current_directory(currentDirectory, size);
+void change_directory(const char *new_directory){
+    chdir(new_directory);
 }
 
 void get_user_command(char* input_buffer, int size){
@@ -32,22 +32,25 @@ void get_user_command(char* input_buffer, int size){
     input_buffer[strcspn(input_buffer, "\n")] = '\0';
 }
 
+void print_whoami(){
+    struct passwd *pw = getpwuid(getuid());
+    printf("%s\n", pw->pw_name);
+}
+
+void change_mode(){}
+
 int main(){
     char machineName[MACHINE_NAME_MAX];
-    char currentDirectory[PATH_NAME_MAX];
     char userInput[USER_INPUT_MAX];
 
     //Setup inicial
     get_hostname(machineName, sizeof(machineName));
-    get_current_directory(currentDirectory, sizeof(currentDirectory));
 
     while (TRUE){
-        print_shell_prompt(machineName, currentDirectory);
+        print_whoami();
+        print_shell_prompt(machineName);
         get_user_command(userInput, sizeof(userInput));
     }
-    change_directory("/tmp", currentDirectory, sizeof(currentDirectory));
-
-    print_shell_prompt(machineName, currentDirectory);
     
     
     exit(0);
