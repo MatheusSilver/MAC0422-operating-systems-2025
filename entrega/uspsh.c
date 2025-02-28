222a99195833eb4fc15be683ce8a3484c6ca2440
Matheus <matheus.feitosa16@outlook.com>
2025-02-28 16:30:59 -0300

    Comandos Internos + inicio de readline

--- entrega/uspsh.c
+++ entrega/uspsh.c
@@ -3,11 +3,14 @@
 #include <unistd.h>
 #include <string.h>
 
+#include <readline/history.h>
+#include <readline/readline.h>
+
 #define MACHINE_NAME_MAX 16
-#define PATH_NAME_MAX 255
-#define USER_INPUT_MAX 255
-#define TRUE 1
-#define FALSE 0
+#define PATH_NAME_MAX 1024
+
+
+#define IFS "\n\t " //IFS padrão (talvez num shell de verdade isso seria uma variável...)
 
 
 void get_hostname(char *hostname, int size) {
@@ -22,34 +25,67 @@ void get_current_directory(char *currentDirectory, int size) {
     getcwd(currentDirectory, size);
 }
 
-void change_directory(const char *new_directory, char *currentDirectory, int size){
-    chdir(new_directory);    
-    get_current_directory(currentDirectory, size);
+
+char* get_user_command(){
+    char* inputBuffer;
+    size_t inputBufferSize;
+    getline(&inputBuffer, &inputBufferSize, stdin);
+    
+    printf("%s", inputBuffer);
+    return inputBuffer;
 }
 
-void get_user_command(char* input_buffer, int size){
-    fgets(input_buffer, size, stdin);
-    input_buffer[strcspn(input_buffer, "\n")] = '\0';
+char** extract_tokens_from_line(char* userInput){
+    char** tokens[];
+    
+    return tokens;
 }
 
 int main(){
     char machineName[MACHINE_NAME_MAX];
     char currentDirectory[PATH_NAME_MAX];
-    char userInput[USER_INPUT_MAX];
-
+    
     //Setup inicial
     get_hostname(machineName, sizeof(machineName));
     get_current_directory(currentDirectory, sizeof(currentDirectory));
-
-    while (TRUE){
+    
+    while (1){
         print_shell_prompt(machineName, currentDirectory);
-        get_user_command(userInput, sizeof(userInput));
+        get_user_command();
     }
     change_directory("/tmp", currentDirectory, sizeof(currentDirectory));
-
+    
     print_shell_prompt(machineName, currentDirectory);
     
     
     exit(0);
-
+    
 }
+
+/*
+>>>>>>>>>>>>>>>>>>>>>>>>> COMANDOS INTERNOS <<<<<<<<<<<<<<<<<<<<<<<<<
+*/
+
+void change_directory(const char *new_directory, char *currentDirectory, int size){
+    chdir(new_directory);    
+    get_current_directory(currentDirectory, size);
+}
+
+void whoami(){
+    struct passwd *pw = getpwuid(getuid());
+    printf("%s\n", pw->pw_name);
+}
+
+/*
+chmod espera um mode_t, mas estou assumindo que é responsabilidade do próprio comando tratar 
+do formato que ele recebe seus dados, neste caso, como padrão, todos sendo uma string.
+*/
+void change_permissions(const char* pathname, const char* mode){
+    int permissions = strtol(mode, NULL, 8);
+    chmod(pathname, permissions);
+}
+
+/*
+>>>>>>>>>>>>>>>>>>>>>>>>> UTILIDADES GERAIS <<<<<<<<<<<<<<<<<<<<<<<<<
+*/
+
