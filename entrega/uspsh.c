f05fa300e06d57c53f0ca2ddab9fe9a695423f30
Matheus <matheus.feitosa16@outlook.com>
2025-03-01 15:38:47 -0300

    Fix Warnings + organização de código

--- entrega/uspsh.c
+++ entrega/uspsh.c
@@ -11,8 +11,11 @@
 #include <readline/history.h>
 #include <readline/readline.h>
 
-// Usado pelo whoami
+// Usado pelo whoami (Converter ID em nome de usuário)
 #include <pwd.h>
+
+//Usado pelo chmod
+#include <sys/stat.h>
 
 #define MACHINE_NAME_MAX 16
 #define PATH_NAME_MAX 1024
@@ -24,47 +27,19 @@
 char machineName[MACHINE_NAME_MAX];
 char currentDirectory[PATH_NAME_MAX];
 
-void get_hostname(char *hostname, int size) {
-    gethostname(hostname, size);
-}
+void get_hostname(char *, int);
+void get_shell_prompt(const char *, const char *, char *, int);
+void get_current_directory(char *, int);
+void get_user_command(char **, char *);
+int extract_tokens_from_line(char *, char *[]);
+void command_handler(char *, char *[]);
 
-void get_shell_prompt(const char *hostname, const char *currentDirectory, char* msgBuffer, int bufferSize) {
-    snprintf(msgBuffer, bufferSize, "[%s:%s]$ ", hostname, currentDirectory);
-}
-
-void get_current_directory(char *currentDirectory, int size) {
-    getcwd(currentDirectory, size);
-}
-
-void get_user_command(char **inputBuffer, char *inputMSG) {
-    *inputBuffer = readline(inputMSG);
-    if (*inputBuffer && **inputBuffer) { //Impedir que um input vazio seja salvo no history
-        add_history(*inputBuffer);
-    }
-}
-
-//Returns numTokens
-int extract_tokens_from_line(char* userInput, char* tokens[]){
-    int numTokens = 0;
-
-
-    char *token = strtok(userInput, IFS);  
-    while (token != NULL) {
-        tokens[numTokens++] = token;
-        token = strtok(NULL, IFS);
-    }
-
-    tokens[numTokens] = NULL;  // Garantir que o último token seja NULL, pra demarcar onde o exec deve parar.
-                               // Talvez isso devesse ser responsabilidade de outra função...
-
-    return numTokens; //Se zero, alguma coisa deu errado e não foi possível capturar tokens.
-}
 
 int main(){
     char *commandLineInput = NULL;
     char *commandTokens[MAX_TOKEN_QTD];
-    char *promptMSG[PATH_NAME_MAX + MACHINE_NAME_MAX + 6]; //Na pratica são 5, mas estamos contando o \0 no final.
-
+    char promptMSG[PATH_NAME_MAX + MACHINE_NAME_MAX + 6]; //Na pratica são 5, mas estamos contando o \0 no final.
+    
     //Setup inicial
     get_hostname(machineName, sizeof(machineName));
     get_current_directory(currentDirectory, sizeof(currentDirectory));
@@ -74,13 +49,13 @@ int main(){
         get_user_command(&commandLineInput, promptMSG);
         int numTokens = extract_tokens_from_line(commandLineInput, commandTokens);
         if (numTokens > 0) { //Provavelmente o usuário só digitou Enter
-            commandHandler(commandTokens[0], commandTokens);
+            command_handler(commandTokens[0], commandTokens);
         }
-
+        
         free(commandLineInput);
     }
-
-    exit_function();
+    
+    return 0;
     
 }
 
@@ -88,7 +63,13 @@ int main(){
 >>>>>>>>>>>>>>>>>>>>>>>>> GERENCIADOR DE COMANDOS <<<<<<<<<<<<<<<<<<<<<<<<<
 */
 
-void commandHandler(char* command, char* arguments[]){
+void change_directory(const char*);
+void whoami();
+void change_permissions(const char*, const char*);
+int execute_external_command(char*, char*[]);
+void print_error(char *);
+
+void command_handler(char* command, char* arguments[]){
     if (strcmp(command, "cd") == 0) {
         change_directory(arguments[1]);
         get_current_directory(currentDirectory, PATH_NAME_MAX);
@@ -114,11 +95,6 @@ void whoami(){
     struct passwd *pw = getpwuid(getuid());
     printf("%s\n", (pw->pw_name));
 }
-
-/*
-chmod espera um mode_t, mas estou assumindo que é responsabilidade do próprio comando tratar 
-do formato que ele recebe seus dados, neste caso, como padrão, todos sendo uma string.
-*/
 void change_permissions(const char* pathname, const char* mode){
     int permissions = strtol(mode, NULL, 8);
     chmod(pathname, permissions);
@@ -137,9 +113,44 @@ int execute_external_command(char* command, char* arguments[]){
 >>>>>>>>>>>>>>>>>>>>>>>>> UTILIDADES GERAIS <<<<<<<<<<<<<<<<<<<<<<<<<
 */
 
-void exit_function(){
-    exit(0);
+void get_hostname(char *hostname, int size) {
+    gethostname(hostname, size);
 }
+
+void get_shell_prompt(const char *hostname, const char *currentDirectory, char* msgBuffer, int bufferSize) {
+    snprintf(msgBuffer, bufferSize, "[%s:%s]$ ", hostname, currentDirectory);
+}
+
+void get_current_directory(char *currentDirectory, int size) {
+    getcwd(currentDirectory, size);
+}
+
+void get_user_command(char **inputBuffer, char *inputMSG) {
+    *inputBuffer = readline(inputMSG);
+    if (*inputBuffer && **inputBuffer) { //Impedir que um input vazio seja salvo no history
+        add_history(*inputBuffer);
+    }
+}
+
+int extract_tokens_from_line(char* userInput, char* tokens[]){
+    int numTokens = 0;
+    
+    
+    char *token = strtok(userInput, IFS);  
+    while (token != NULL) {
+        tokens[numTokens++] = token;
+        token = strtok(NULL, IFS);
+    }
+    
+    tokens[numTokens] = NULL;  // Garantir que o último token seja NULL, pra demarcar onde o exec deve parar.
+    // Talvez isso devesse ser responsabilidade de outra função...
+    
+    return numTokens; //Se zero, alguma coisa deu errado e não foi possível capturar tokens.
+}
+
+/*
+>>>>>>>>>>>>>>>>>>>>>>>>> FEEDBACK DE ERROS <<<<<<<<<<<<<<<<<<<<<<<<<
+*/
 
 void print_error(char* commandName) {
     printf("O comando %s não foi reconhecido\n", commandName);
