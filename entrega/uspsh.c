2949a29dd28aa7e6bbf14e9e6a1d2d1afa55fb75
Matheus <matheus.feitosa16@outlook.com>
2025-03-01 13:03:13 -0300

    Implementação de funções básicas + fix do cd e command_parser

--- entrega/uspsh.c
+++ entrega/uspsh.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+//Usado para as syscalls
 #include <unistd.h>
+
+//Usado para separação de tokens e conversão do input para chmod
 #include <string.h>
 
+//Usado na captura de commandos e funcionalidade do histórico.
 #include <readline/history.h>
 #include <readline/readline.h>
 
+// Usado pelo whoami
+#include <pwd.h>
+
 #define MACHINE_NAME_MAX 16
 #define PATH_NAME_MAX 1024
-
+#define MAX_TOKEN_QTD 25 //Improvável, mas vai que né?
 
 #define IFS "\n\t " //IFS padrão (talvez num shell de verdade isso seria uma variável...)
 
+
+char machineName[MACHINE_NAME_MAX];
+char currentDirectory[PATH_NAME_MAX];
 
 void get_hostname(char *hostname, int size) {
     gethostname(hostname, size);
@@ -26,54 +37,79 @@ void get_current_directory(char *currentDirectory, int size) {
 }
 
 
-char* get_user_command(){
-    char* inputBuffer;
-    size_t inputBufferSize;
-    getline(&inputBuffer, &inputBufferSize, stdin);
-    
-    printf("%s", inputBuffer);
-    return inputBuffer;
+void get_user_command(char **inputBuffer) {
+    size_t inputBufferSize = 0; 
+    getline(inputBuffer, &inputBufferSize, stdin);
 }
 
-char** extract_tokens_from_line(char* userInput){
-    char** tokens[];
-    
-    return tokens;
+//Returns numTokens
+int extract_tokens_from_line(char* userInput, char* tokens[]){
+    int numTokens = 0;
+
+
+    char *token = strtok(userInput, IFS);  
+    while (token != NULL) {
+        tokens[numTokens++] = token;
+        token = strtok(NULL, IFS);
+    }
+
+    tokens[numTokens] = NULL;  // Garantir que o último token seja NULL, pra demarcar onde o exec deve parar.
+                               // Talvez isso devesse ser responsabilidade de outra função...
+
+    return numTokens; //Se zero, alguma coisa deu errado e não foi possível capturar tokens.
 }
 
 int main(){
-    char machineName[MACHINE_NAME_MAX];
-    char currentDirectory[PATH_NAME_MAX];
-    
+    char *commandLineInput = NULL;
+    char *commandTokens[MAX_TOKEN_QTD];
+
     //Setup inicial
     get_hostname(machineName, sizeof(machineName));
     get_current_directory(currentDirectory, sizeof(currentDirectory));
     
     while (1){
         print_shell_prompt(machineName, currentDirectory);
-        get_user_command();
+        get_user_command(&commandLineInput);
+        int numTokens = extract_tokens_from_line(commandLineInput, commandTokens);
+        if (numTokens > 0) { //Provavelmente o usuário só digitou Enter
+            commandHandler(commandTokens[0], commandTokens);
+        }
     }
-    change_directory("/tmp", currentDirectory, sizeof(currentDirectory));
+
+    free(commandLineInput);
+    exit_function();
     
-    print_shell_prompt(machineName, currentDirectory);
-    
-    
-    exit(0);
-    
+}
+
+/*
+>>>>>>>>>>>>>>>>>>>>>>>>> GERENCIADOR DE COMANDOS <<<<<<<<<<<<<<<<<<<<<<<<<
+*/
+
+void commandHandler(char* command, char* arguments[]){
+    if (strcmp(command, "cd") == 0) {
+        change_directory(arguments[1]);
+        get_current_directory(currentDirectory, PATH_NAME_MAX);
+    } else if (strcmp(command, "whoami") == 0){
+        whoami();
+    } else if (strcmp(command, "chmod") == 0){
+        change_permissions(arguments[1], arguments[2]);
+    } else {
+        int executionResult = execute_external_command(command, arguments);
+        if (executionResult != 0) { print_error(command); }
+    }
 }
 
 /*
 >>>>>>>>>>>>>>>>>>>>>>>>> COMANDOS INTERNOS <<<<<<<<<<<<<<<<<<<<<<<<<
 */
 
-void change_directory(const char *new_directory, char *currentDirectory, int size){
+void change_directory(const char *new_directory) {
     chdir(new_directory);    
-    get_current_directory(currentDirectory, size);
 }
 
 void whoami(){
     struct passwd *pw = getpwuid(getuid());
-    printf("%s\n", pw->pw_name);
+    printf("%s\n", (pw->pw_name));
 }
 
 /*
@@ -86,6 +122,22 @@ void change_permissions(const char* pathname, const char* mode){
 }
 
 /*
+>>>>>>>>>>>>>>>>>>>>>>>>> EXECUÇÃO DE COMANDOS <<<<<<<<<<<<<<<<<<<<<<<<<
+*/
+
+int execute_external_command(char* command, char* arguments[]){
+    //Aqui terá a implementação do fork exec e wait
+    return 0;
+}
+
+/*
 >>>>>>>>>>>>>>>>>>>>>>>>> UTILIDADES GERAIS <<<<<<<<<<<<<<<<<<<<<<<<<
 */
 
+void exit_function(){
+    exit(0);
+}
+
+void print_error(char* commandName) {
+    printf("O comando %s não foi reconhecido\n", commandName);
+}
