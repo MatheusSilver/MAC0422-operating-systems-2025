4a8f050af47a3c15e912f072924aa4ec89305b9c
Matheus <matheus.feitosa16@outlook.com>
2025-03-02 18:25:55 -0300

    ep1 - Correção de parsing do trace + testes iniciais

--- entrega/ep1.c
+++ entrega/ep1.c
@@ -20,45 +20,49 @@ typedef struct {
     int deadline;
 } SimulatedProcessData;
 
+//Lembrar de organizar isso depois.
+int open_file(FILE **, char*, char *);
+int read_next_line(FILE *, char*);
+int close_file(FILE *);
+int parse_line_data(char *, SimulatedProcessData *);
+void set_process_name(SimulatedProcessData *, const char *);
+void set_process_arrival_time(SimulatedProcessData *, const char *);
+void set_process_execution_time(SimulatedProcessData *, const char *);
+void set_process_deadline(SimulatedProcessData *, const char *);
+int get_array_of_processes(FILE *, SimulatedProcessData*[]);
+
 int main(int args, char* argv[]){
     if (args != 2){
-        printf("Execute este programa usando %s <nome_do_arquivo_de_trace>\n", argv[0]);
+        printf("Execute este programa usando %s <escalonador a simular> <nome do arquivo de trace> <nome do arquivo de saida>\n", argv[0]);
         exit(-1);
     }
 
     FILE *fileDescriptor;
-    char * lineBuffer[MAX_LINE_SIZE];
-    SimulatedProcessData * processList[MAX_SIMULATED_PROCESSES];
-    int appendedProcesses = 0;
+    SimulatedProcessData *processList[MAX_SIMULATED_PROCESSES];
 
-    if (open_file(fileDescriptor, argv[1])){exit(-1);}
+    if (open_file(&fileDescriptor, argv[1], "r")){exit(-1);}
+
+    int numProcesses = get_array_of_processes(fileDescriptor, processList);
     
-    while (read_next_line(fileDescriptor, lineBuffer))
-    {
-        SimulatedProcessData *process;
-        if (parse_line_data(lineBuffer, process)){
-            printf("Há um erro na linha %d do arquivo de trace", (appendedProcesses+1));
-            exit(-1);
-        }
-        processList[appendedProcesses++] = process;
+    //Testando, lembrar de tirar depois.
+    for (int i = 0; i < numProcesses; i++) {
+        printf("%s\n", processList[i]->name);
     }
     
+    close_file(fileDescriptor);
 
-
-    
-
-
+    exit(0);
 }
+
 
 /*
 >>>>>>>>>>>>>>>>>>>>>>>>> UTILIDADES DE ARQUIVO <<<<<<<<<<<<<<<<<<<<<<<<<
 */
 
 
-int open_file(FILE * fileDescriptor, char* filepath){
-    fileDescriptor = fopen(filepath, "r");
-
-    if (fileDescriptor == NULL) {
+int open_file(FILE **fileDescriptor, char *filepath, char *fileMode) {
+    *fileDescriptor = fopen(filepath, fileMode);
+    if (*fileDescriptor == NULL) {
         printf("O arquivo %s não pode ser aberto\n", filepath);
         return -1;
     }
@@ -67,20 +71,20 @@ int open_file(FILE * fileDescriptor, char* filepath){
 }
 
 int read_next_line(FILE * fileDescriptor, char* lineBuffer){
-    if (fgets(lineBuffer, sizeof(lineBuffer), fileDescriptor) == NULL) {
-        return -1; // Acabou o arquivo ou algo deu errado.
+    if (fgets(lineBuffer, MAX_LINE_SIZE, fileDescriptor) == NULL) {
+        return 0; // Acabou o arquivo.
     }
-    
-    return 0; // Algo foi lido
+
+    return 1; // Algo foi lido
 }
 
 int close_file(FILE * fileDescriptor){
     if (fclose(fileDescriptor)) {
         printf("Não foi possível fechar o arquivo\n");
-        return -1;
+        return 0;
     }
 
-    return 0;
+    return -1;
 }
 
 /*
@@ -96,15 +100,36 @@ int parse_line_data(char * line, SimulatedProcessData * process){
         set_process_execution_time,
         set_process_deadline
     };
-    
     char * parsed_data = strtok(line, FILE_SEPARATOR);  
     while (parsed_data != NULL) {
-        set_process_values[currentParsedData++](process, parsed_data);
+        if (currentParsedData < 4) {
+            set_process_values[currentParsedData](process, parsed_data);
+        }
         parsed_data = strtok(NULL, FILE_SEPARATOR);
+        currentParsedData++;
     }
-    
-    return currentParsedData == 4; //Se diferente de 4, então alguma coisa deu bem errado.
+
+    return currentParsedData != 4; //Se diferente de 4, então alguma coisa deu bem errado.
 }
+
+int get_array_of_processes(FILE * fileDescriptor, SimulatedProcessData * processList[]){
+    char lineBuffer[MAX_LINE_SIZE];
+    int appendedProcesses = 0;
+
+    while (read_next_line(fileDescriptor, lineBuffer)) {
+        SimulatedProcessData *process = (SimulatedProcessData *)malloc(sizeof(SimulatedProcessData));
+        
+        if (parse_line_data(lineBuffer, process)) {
+            printf("Há um erro na linha %d do arquivo de trace\n", (appendedProcesses+1));
+            exit(-1);
+        }
+
+        processList[appendedProcesses++] = process;
+    }
+
+    return appendedProcesses;
+}
+
 
 /*
 >>>>>>>>>>>>>>>>>>>>>>>>> UTILIDADES DO STRUCT <<<<<<<<<<<<<<<<<<<<<<<<<
